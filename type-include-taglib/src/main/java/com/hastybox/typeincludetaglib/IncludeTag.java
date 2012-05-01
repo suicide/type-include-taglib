2bd2b259eec03f3e604050214b7f7181a365a5fd
Patrick Sy <patrick.sy@get-it.us>
2012-05-01 17:24:17 +0200

    code clean up

--- type-include-taglib/src/main/java/com/hastybox/typeincludetaglib/IncludeTag.java
+++ type-include-taglib/src/main/java/com/hastybox/typeincludetaglib/IncludeTag.java
@@ -5,7 +5,6 @@ package com.hastybox.typeincludetaglib;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +12,6 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
@@ -23,16 +21,57 @@ import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.TagSupport;
 
 /**
+ * <p>
+ * The Include Tag class. It accepts an object that is given in the
+ * <code>self</code> member and a template modifier stored in the
+ * <code>template</code> member. The <code>self</code> parameter is manadatory
+ * while the <code>template</code> parameter is optional.
+ * </p>
+ * 
+ * <p>
+ * In the current implementation all templates to use must be stored in
+ * <code>/WEB-INF/typeTemplates/</code> followed by a folder with the package
+ * name and finally the jsp file with the class name. For example:
+ * </p>
+ * 
+ * <pre>
+ * /WEB-INF/typeTemplates/java.lang/String.jsp
+ * /WEB-INF/typeTemplates/com.hastybox.blubb/Article.teaser.jsp
+ * </pre>
+ * 
  * @author psy
  * 
  */
 public class IncludeTag extends TagSupport {
 
+	/**
+	 * ServletResponseWrapper that fetches the output in order to include it in
+	 * the original response later on.
+	 * 
+	 * @author psy
+	 * 
+	 */
 	private static final class OutputWrappingHttpServletResponseWrapper extends
 			HttpServletResponseWrapper {
+
+		/**
+		 * ServletOutputStream that stores the output in a ByteArrayOutputStream
+		 * for further usage.
+		 * 
+		 * @author psy
+		 * 
+		 */
 		private static final class StoringServletOutputStream extends
 				ServletOutputStream {
-			private ByteArrayOutputStream out = new ByteArrayOutputStream();
+			
+			/**
+			 * simple ByteArrayOutputStream to fetch output
+			 */
+			private ByteArrayOutputStream out;
+			
+			public StoringServletOutputStream() {
+				out = new ByteArrayOutputStream();
+			}
 
 			@Override
 			public void write(int b) throws IOException {
@@ -45,18 +84,23 @@ public class IncludeTag extends TagSupport {
 			public ByteArrayOutputStream getOut() {
 				return out;
 			}
-			
+
 		}
 
+		/**
+		 * Dummy ServletOutputStream
+		 */
 		private ServletOutputStream out;
+		
+		/**
+		 * Writer wrapped arout OutputStream
+		 */
 		private PrintWriter writer;
 
 		private OutputWrappingHttpServletResponseWrapper(
 				HttpServletResponse response) {
 			super(response);
-		}
-
-		public void init() {
+			// initialization
 			out = new StoringServletOutputStream();
 			
 			writer = new PrintWriter(out);
@@ -71,7 +115,7 @@ public class IncludeTag extends TagSupport {
 		public PrintWriter getWriter() throws IOException {
 			return writer;
 		}
-		
+
 		public ByteArrayOutputStream getOut() {
 			return ((StoringServletOutputStream) out).getOut();
 		}
@@ -79,19 +123,20 @@ public class IncludeTag extends TagSupport {
 	}
 
 	/**
-	 * Manages given attributes instead of attributes from original Request when available
+	 * Manages given attributes instead of attributes from original Request when
+	 * available
 	 * 
 	 * @author psy
-	 *
+	 * 
 	 */
-	static private final class AttributeWrappingHttpServletRequestWrapper extends
-			HttpServletRequestWrapper {
+	static private final class AttributeWrappingHttpServletRequestWrapper
+			extends HttpServletRequestWrapper {
 		private final Map<String, Object> wrappingAttributes;
 
 		private AttributeWrappingHttpServletRequestWrapper(
 				HttpServletRequest request) {
 			super(request);
-			
+
 			// initialize
 			wrappingAttributes = new HashMap<String, Object>();
 		}
@@ -117,17 +162,17 @@ public class IncludeTag extends TagSupport {
 	 * serial id
 	 */
 	private static final long serialVersionUID = 1561430403921500250L;
-	
+
 	/**
 	 * base path for type templates to include
 	 */
 	private static final String BASEPATH = "/WEB-INF/typeTemplates/";
-	
+
 	/**
 	 * object to render
 	 */
 	private Object self;
-	
+
 	/**
 	 * template to use for rendering
 	 */
@@ -155,13 +200,16 @@ public class IncludeTag extends TagSupport {
 	public int doStartTag() throws JspException {
 
 		try {
-			ServletRequest req = new AttributeWrappingHttpServletRequestWrapper((HttpServletRequest) pageContext.getRequest());
-			req.setAttribute("self", self);
+			ServletRequest req = new AttributeWrappingHttpServletRequestWrapper(
+					(HttpServletRequest) pageContext.getRequest());
 			
+			// set attributes to "new" context
+			req.setAttribute("self", self);
+
 			// create path to jsp to be included
 			StringBuilder pathBuilder = new StringBuilder();
 			pathBuilder.append(BASEPATH);
-			
+
 			// find package
 			Package selfPackage = self.getClass().getPackage();
 			if (selfPackage != null) {
@@ -170,26 +218,27 @@ public class IncludeTag extends TagSupport {
 			// add class name
 			pathBuilder.append("/");
 			pathBuilder.append(self.getClass().getSimpleName());
-			
+
 			// add template if set
 			if (template != null) {
 				pathBuilder.append(".");
 				pathBuilder.append(template);
 			}
 			pathBuilder.append(".jsp");
-			
-			OutputWrappingHttpServletResponseWrapper res = new OutputWrappingHttpServletResponseWrapper((HttpServletResponse) pageContext.getResponse());
-			res.init();
-			
-			pageContext.getRequest().getRequestDispatcher(pathBuilder.toString())
+
+			OutputWrappingHttpServletResponseWrapper res = new OutputWrappingHttpServletResponseWrapper(
+					(HttpServletResponse) pageContext.getResponse());
+
+			pageContext.getRequest()
+					.getRequestDispatcher(pathBuilder.toString())
 					.include(req, res);
-			
+
 			// write stuff to outStream instead of flushing pageContext.getOut()
 			res.getWriter().flush();
-			
+
+			// write to response
 			pageContext.getOut().write(res.getOut().toString());
-			//pageContext.getResponse().getWriter().write(res.getOut().toString());
-			
+
 		} catch (ServletException e) {
 			throw new JspException(e);
 		} catch (IOException e) {
@@ -198,21 +247,23 @@ public class IncludeTag extends TagSupport {
 
 		return SKIP_BODY;
 	}
-	
+
 	@Override
 	public int doEndTag() throws JspException {
 		return EVAL_PAGE;
 	}
 
 	/**
-	 * @param self the self to set
+	 * @param self
+	 *            the self to set
 	 */
 	public void setSelf(Object self) {
 		this.self = self;
 	}
 
 	/**
-	 * @param template the template to set
+	 * @param template
+	 *            the template to set
 	 */
 	public void setTemplate(String template) {
 		this.template = template;
